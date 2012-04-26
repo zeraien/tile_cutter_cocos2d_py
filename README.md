4b1a421b549fd9d9ca777d8169b70f26337c6f2c
Dmitri Fedortchenko <d@angelhill.net>
2012-04-27 01:00:11 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-tile_cutter_cocos2d_py
+Tile Cutter
 ======================
 
-Image tile cutter for cocos2d
+Because I couldn't not get the famed https://github.com/jlamarche/Tile-Cutter and https://github.com/psineur/Tile-Cutter/network to work in Mac OS X 10.7, with PNG images,
+I googled around and found a simply python implementation.
+
+Improving upon it, and adding support for creation of cocos2d-esque plist files, here we are.
+
+Huh?
+========================
+
+This will take an image, and cut it into a number of tiles. It will also create a plist file containing references to these tiles that can be read by CCBigImage.
+
+More about CCBigImage here: https://github.com/cocos2d/cocos2d-iphone-extensions/tree/develop/Extensions/CCBigImage
+
+The output will be PNG files and one plist file.
+
+Dependencies
+========================
+Required python libraries: Python Image Library (aka Image, PIL).
+
+
+Idea and some code was found here:http://blog.odonnell.nu/posts/creating-tiles-image-python-and-pil/
+
+Usage
+=======================
+
+    Usage: usage: tilecut.py [options]
+
+    Options:
+      -h, --help            show this help message and exit
+      -o NAME, --output=NAME
+                            output name, sans extension (may include directory)
+      -s SUFFIX, --suffix=SUFFIX
+                            suffix for file names, such as '-hd'. Please use
+                            quotes if your suffix includes a dash.
+      -i FILE, --input=FILE
+                            input file
+      -x VALUE, --tileWidth=VALUE
+                            tile width
+      -y VALUE, --tileHeight=VALUE
+                            tile height
+### Example
+    
+    ../tilecut.py -i ../originals/ship_panorama-hd.png -x 424 -y 320 -o ship_panorama -s "-hd"
+    
+
+Will create the files you are looking for.
+
+
+
+Limitations
+========================
+For now, all tiles must be identical in size (rigid), however height and width can be different, if need be.
+
+Feel free to fork and improve!
---
+++ tilecut.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import Image
+import sys
+import optparse
+import os
+
+def cut_it(input_filename, output_filename, tile_width, tile_height, suffix):
+    image = Image.open(input_filename)
+    if not suffix: suffix = ''
+    suffix = suffix.strip("\s\t\"\'\n")
+    
+    tiles = []
+    tile_dict = {
+        'filename': os.path.basename(input_filename),
+        'width':image.size[0],
+        'height':image.size[1],
+        'tiles':tiles
+    }
+    
+    if image.size[0] % tile_width == 0 and image.size[1] % tile_height ==0 :
+        row = 0
+        currentx = 0
+        currenty = 0
+        while currenty < image.size[1]:
+            column = 0
+            while currentx < image.size[0]:
+                print currentx,",",currenty
+                tile = image.crop((currentx,currenty,currentx + tile_width,currenty + tile_height))
+                
+                tile_filename = "%s_%s_%s%s.png" % (output_filename, row, column, suffix)
+                tile.save(tile_filename,"PNG")
+                
+                tiles.append({
+                    'filename':tile_filename,
+                    'rect':'{{%s,%s},{%s,%s}}' % (currentx,currenty,tile_width, tile_height)
+                })
+                
+                currentx += tile_width
+                column+=1
+            currenty += tile_height
+            currentx = 0
+            row+=1
+    else:
+        print "sorry your image does not fit neatly into",tile_width,"*",tile_height,"tiles"
+        sys.exit(1)
+        
+    return tile_dict
+    
+def make_plist(tile_dict):
+    
+    source_bit = """<key>Filename</key>
+<string>%(filename)s</string>
+<key>Size</key>
+<string>{%(width)d, %(height)d}</string>
+"""
+    tile_bit = """<dict>
+	<key>Name</key>
+	<string>%(filename)s</string>
+	<key>Rect</key>
+	<string>%(rect)s</string>
+</dict>
+"""
+    
+    plist_structure = """<?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+    	<key>Source</key>
+    	<dict>%(source_bit)s</dict>
+    	<key>Tiles</key>
+    	<array>%(tile_bits)s</array>
+    </dict>
+    </plist>
+"""
+    source_bit = source_bit % tile_dict
+    
+    tile_bits = []
+    for tile_data in tile_dict['tiles']:
+        tile_bits.append(tile_bit % tile_data)
+    plist_structure = plist_structure % {'source_bit':source_bit, 'tile_bits':tile_bits}
+    return plist_structure
+    
+if __name__=="__main__":
+    parser = optparse.OptionParser()
+    parser.usage = "usage: %prog [options]"
+    
+    parser.add_option("-o", "--output", dest="output_filename",
+                      help="output name, sans extension (may include directory)", metavar="NAME")
+    parser.add_option("-s", "--suffix", dest="suffix",
+                  help="suffix for file names, such as '-hd'. Please use quotes if your suffix includes a dash.", metavar="SUFFIX")
+    parser.add_option("-i", "--input", dest="input_filename",
+                    help="input file", metavar="FILE")
+    parser.add_option("-x", "--tileWidth", metavar="VALUE", dest="tile_width",
+                      help="tile width", type="int")
+    parser.add_option("-y", "--tileHeight", metavar="VALUE", dest="tile_height",
+                    help="tile height", type="int")
+                      
+    (options, args) = parser.parse_args()
+    
+    if not options.input_filename:
+        parser.error("no input file specified")
+    if not options.output_filename:
+        parser.error("no output file specified")
+    if int(options.tile_height) <= 0:
+        parser.error("invalid tile height %s" % options.tile_height)
+    if int(options.tile_width) <= 0:
+        parser.error("invalid tile width %s" % options.tile_width)
+    if not os.path.exists(os.path.abspath(options.input_filename)):
+        options.input_filename =  os.path.abspath(options.input_filename)
+        parser.error("input file '%s' not found" % options.input_filename)
+
+    plist_filename = '%s.plist' % options.output_filename
+    if os.path.exists(os.path.abspath(plist_filename)):
+        parser.error("file exists: %s" % plist_filename)
+        
+    tile_dict = cut_it(**options.__dict__)
+    
+        
+    with open(plist_filename, 'w') as f:
+        f.write(make_plist(tile_dict))
